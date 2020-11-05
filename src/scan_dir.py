9e60f1526e2d8936e09f5ea021a88b737eb55f68
林宏文(Tommy Lin) <tommy.hw.lin@foxconn.com>
2020-11-05 17:34:30 +0800

    Increase scan images to 4

--- src/scan_dir.py
+++ src/scan_dir.py
@@ -17,13 +17,17 @@ target_dir = "../images/group/"
 image1 = mpimg.imread(target_dir + "fly1.jpg")
 image2 = mpimg.imread(target_dir + "fly2.jpg")
 image3 = mpimg.imread(target_dir + "fly3.jpg")
+image4 = mpimg.imread(target_dir + "fly4.jpg")
 
 s12 = structural_similarity(image1, image2, multichannel=True)
-s23 = structural_similarity(image2, image3, multichannel=True)
 s13 = structural_similarity(image1, image3, multichannel=True)
+s14 = structural_similarity(image1, image4, multichannel=True)
+s23 = structural_similarity(image2, image3, multichannel=True)
+s24 = structural_similarity(image2, image4, multichannel=True)
+s34 = structural_similarity(image3, image4, multichannel=True)
 
 
-print("s12={0:2.4f}".format(s12))
-print("s23={0:2.4f}".format(s23))
-print("s13={0:2.4f}".format(s13))
+print("s1x {0:2.4f} {0:2.4f} {0:2.4f}".format(s12, s13, s14))
+print("s2x          {0:2.4f} {0:2.4f}".format(s23, s24))
+print("s3x                   {0:2.4f}".format(s34))
 
