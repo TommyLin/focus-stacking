c0e9649c7dfeb530de98f15496554ed2bb823027
林宏文(Tommy Lin) <gnat_lin@hotmail.com>
2020-11-21 23:04:47 +0800

    Use modern filesystem api to scan files in folder

--- src/scan_dir.cpp
+++ src/scan_dir.cpp
@@ -1,7 +1,22 @@
+#include <string>
+#include <iostream>
+#include <experimental/filesystem>
 
+namespace fs = std::experimental::filesystem;
 
 
 int main(int argc, char *argv[])
 {
+    std::string path = "../images/group/";
+    std::cout << argc << std::endl;
+    if (argc > 1) {
+        path = argv[1];
+        std::cout << argv[1] << std::endl;
+    } else {
+        path = "../images/group/";
+    }
+    for (const auto & entry : fs::directory_iterator(path))
+        std::cout << entry.path() << std::endl;
+
     return 0;
 }
