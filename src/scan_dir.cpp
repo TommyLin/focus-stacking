d96e6c0796a85fbfbca3cc9ccb60e1887e68ca67
林宏文(Tommy Lin) <gnat_lin@hotmail.com>
2020-11-21 23:14:18 +0800

    Remove duplicated initial value
    Add DEBUG flag to isolate debug message output

--- src/scan_dir.cpp
+++ src/scan_dir.cpp
@@ -7,14 +7,19 @@ namespace fs = std::experimental::filesystem;
 
 int main(int argc, char *argv[])
 {
-    std::string path = "../images/group/";
+    std::string path;
+
+#ifdef DEBUG
     std::cout << argc << std::endl;
+#endif
+
     if (argc > 1) {
         path = argv[1];
         std::cout << argv[1] << std::endl;
     } else {
         path = "../images/group/";
     }
+
     for (const auto & entry : fs::directory_iterator(path))
         std::cout << entry.path() << std::endl;
 
