9e94e890e67679f262c856fc6dc54e5e407719e5
林宏文(Tommy Lin) <gnat_lin@hotmail.com>
2020-11-21 23:40:58 +0800

    Exam directory exist or not

--- src/scan_dir.cpp
+++ src/scan_dir.cpp
@@ -13,11 +13,11 @@ int main(int argc, char *argv[])
     std::cout << argc << std::endl;
 #endif
 
-    if (argc > 1) {
+    if ((argc > 1) && fs::exists(argv[1])) {
         path = argv[1];
         std::cout << argv[1] << std::endl;
     } else {
-        path = "../images/group/";
+        path = "./";
     }
 
     for (const auto & entry : fs::directory_iterator(path))
