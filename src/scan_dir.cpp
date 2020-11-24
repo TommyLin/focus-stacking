ef04f8e2049bcb191736ace4487d4053e0447075
林宏文(Tommy Lin) <gnat_lin@hotmail.com>
2020-11-24 19:47:20 +0800

    Include scan_dir to libray not stand alone executable

--- src/scan_dir.cpp
+++ src/scan_dir.cpp
@@ -1,27 +1,25 @@
-#include <string>
-#include <iostream>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <experimental/filesystem>
 
 namespace fs = std::experimental::filesystem;
 
 
-int main(int argc, char *argv[])
+int scan_dir(char *path)
 {
-    std::string path;
-
 #ifdef DEBUG
-    std::cout << argc << std::endl;
+    std::cout << path << std::endl;
 #endif
 
-    if ((argc > 1) && fs::exists(argv[1])) {
-        path = argv[1];
-        std::cout << argv[1] << std::endl;
-    } else {
-        path = "./";
-    }
+    if (!fs::exists(path))
+        throw std::invalid_argument("Input path not exist!");
 
     for (const auto & entry : fs::directory_iterator(path))
         std::cout << entry.path() << std::endl;
 
     return 0;
+}
+
+PYBIND11_MODULE(focus_stacking, m) {
+    m.def("scan_dir", &scan_dir, "Scan image files in folder");
 }
