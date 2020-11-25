5f85355e8d1745a49cc9a201358323da2dcccefa
林宏文(Tommy Lin) <tommy.hw.lin@foxconn.com>
2020-11-25 09:29:36 +0800

    Collect header files to $(PRJ_ROOT)/inc

--- src/focus_stacking.cpp
+++ src/focus_stacking.cpp
@@ -1,7 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "focus_stacking.h"
+#include "focus_stacking.hpp"
 
 PYBIND11_MODULE(focus_stacking, m) {
     m.def("scan_dir",      &scan_dir,      "Scan image files in folder");
