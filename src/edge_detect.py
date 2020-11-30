69f7ddb65f5f4f431952fcf4dac5a0419118e013
林宏文(Tommy Lin) <tommy.hw.lin@foxconn.com>
2020-11-30 16:41:35 +0800

    Try to fix github superlint warnings

--- src/edge_detect.py
+++ src/edge_detect.py
@@ -4,13 +4,15 @@ import sys
 import matplotlib.image as mpimg
 from skimage import filters
 
-def edge_detect(fin = "../images/fly1.jpg", fout = "fly_edge.jpg"):
+
+def edge_detect(fin="../images/fly1.jpg", fout="fly_edge.jpg"):
     if fin == "" or fout == "":
-        raise SyntaxError('No parameter')
+        raise SyntaxError("No parameter")
 
     image = mpimg.imread(fin)
     edges = filters.sobel(image)
     mpimg.imsave(fout, edges)
 
-#@pytest.mark.skip
-#edge_detect(sys.argv[1], sys.argv[2])
+# @pytest.mark.skip
+# edge_detect(sys.argv[1], sys.argv[2])
+
