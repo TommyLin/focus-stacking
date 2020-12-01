f16ca604bc01b77f0914cf43cb18711d1bbea9ff
林宏文(Tommy Lin) <tommy.hw.lin@foxconn.com>
2020-12-01 11:55:49 +0800

    Fix Super-Lint warnings

--- contrib/install.sh
+++ contrib/install.sh
@@ -13,25 +13,25 @@ install() {
   pkgname=$2
   pkgbranch=$3
   pkgfull=$4
-  cmakeargs="${@:5}"
-  pkgrepo=https://github.com/$githuborg/$pkgname.git
+  cmakeargs="${*:5}"
+  #pkgrepo=https://github.com/$githuborg/$pkgname.git
   repotar=https://github.com/$githuborg/$pkgname/archive/$pkgbranch.tar.gz
 
   workdir=$(mktemp -d /tmp/build.XXXXXXXXX)
   echo "Work directory: $workdir"
-  mkdir -p $workdir
-  pushd $workdir
+  mkdir -p "$workdir"
+  pushd "$workdir" || return 1
   echo "remote tarball: $repotar"
-  curl -sSL -o $pkgfull.tar.gz $repotar
-  rm -rf $pkgfull
-  tar xf $pkgfull.tar.gz
-  cd $pkgfull
+  curl -sSL -o "$pkgfull".tar.gz "$repotar"
+  rm -rf "$pkgfull"
+  tar xf "$pkgfull".tar.gz
+  cd "$pkgfull" || exit
   mkdir -p build
-  cd build
-  cmake $cmakeargs ..
+  cd build || exit
+  cmake "$cmakeargs" ..
   make install
-  popd
-  rm -rf $workdir
+  popd || return 2
+  rm -rf "$workdir"
 
 }
 
@@ -39,10 +39,10 @@ pybind11() {
 
   cmakeargs=("-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}")
   cmakeargs+=("-DCMAKE_BUILD_TYPE=Release")
-  cmakeargs+=("-DPYTHON_EXECUTABLE:FILEPATH=`which python3`")
+  cmakeargs+=("-DPYTHON_EXECUTABLE:FILEPATH=$(which python3)")
   cmakeargs+=("-DPYBIND11_TEST=OFF")
-  install ${PYBIND_ORG:-pybind} pybind11 ${PYBIND_BRANCH:-v2.4.3} \
-    ${PYBIND_LOCAL:-pybind11-2.4.3} "${cmakeargs[@]}"
+  install "${PYBIND_ORG:-pybind}" pybind11 "${PYBIND_BRANCH:-v2.4.3}" \
+    "${PYBIND_LOCAL:-pybind11-2.4.3}" "${cmakeargs[@]}"
 
 }
 
@@ -50,8 +50,8 @@ xtl() {
 
   cmakeargs=("-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}")
   cmakeargs+=("-DCMAKE_BUILD_TYPE=Release")
-  install ${XTL_ORG:-xtensor-stack} xtl ${XTL_BRANCH:-0.6.9} \
-    ${XTL_LOCAL:-xtl-0.6.9} "${cmakeargs[@]}"
+  install "${XTL_ORG:-xtensor-stack}" xtl "${XTL_BRANCH:-0.6.9}" \
+    "${XTL_LOCAL:-xtl-0.6.9}" "${cmakeargs[@]}"
 
 }
 
@@ -60,8 +60,8 @@ xsimd() {
   cmakeargs=("-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}")
   cmakeargs+=("-DCMAKE_BUILD_TYPE=Release")
   cmakeargs+=("-DBUILD_TESTS=OFF")
-  install ${XSIMD_ORG:-xtensor-stack} xsimd ${XSIMD_BRANCH:-7.4.4} \
-    ${XSIMD_LOCAL:-xsimd-7.4.4} "${cmakeargs[@]}"
+  install "${XSIMD_ORG:-xtensor-stack}" xsimd "${XSIMD_BRANCH:-7.4.4}" \
+    "${XSIMD_LOCAL:-xsimd-7.4.4}" "${cmakeargs[@]}"
 
 }
 
@@ -69,8 +69,8 @@ xtensor() {
 
   cmakeargs=("-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}")
   cmakeargs+=("-DCMAKE_BUILD_TYPE=Release")
-  install ${XTENSOR_ORG:-xtensor-stack} xtensor ${XTENSOR_BRANCH:-0.21.3} \
-    ${XTENSOR_LOCAL:-xtensor-0.21.3} "${cmakeargs[@]}"
+  install "${XTENSOR_ORG:-xtensor-stack}" xtensor "${XTENSOR_BRANCH:-0.21.3}" \
+    "${XTENSOR_LOCAL:-xtensor-0.21.3}" "${cmakeargs[@]}"
 
 }
 
@@ -78,9 +78,9 @@ xtensor_blas() {
 
   cmakeargs=("-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}")
   cmakeargs+=("-DCMAKE_BUILD_TYPE=Release")
-  install ${XTENSOR_BLAS_ORG:-xtensor-stack} xtensor-blas \
-    ${XTENSOR_BLAS_BRANCH:-0.17.1} \
-    ${XTENSOR_BLAS_LOCAL:-xtensor-blas-0.17.1} "${cmakeargs[@]}"
+  install "${XTENSOR_BLAS_ORG:-xtensor-stack}" xtensor-blas \
+    "${XTENSOR_BLAS_BRANCH:-0.17.1}" \
+    "${XTENSOR_BLAS_LOCAL:-xtensor-blas-0.17.1}" "${cmakeargs[@]}"
 
 }
 
@@ -88,25 +88,25 @@ xtensor_python() {
 
   cmakeargs=("-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}")
   cmakeargs+=("-DCMAKE_BUILD_TYPE=Release")
-  install ${XTENSOR_PYTHON_ORG:-xtensor-stack} xtensor-python \
-    ${XTENSOR_PYTHON_BRANCH:-0.24.1} \
-    ${XTENSOR_PYTHON_LOCAL:-xtensor-python-0.24.1} "${cmakeargs[@]}"
+  install "${XTENSOR_PYTHON_ORG:-xtensor-stack}" xtensor-python \
+    "${XTENSOR_PYTHON_BRANCH:-0.24.1}" \
+    "${XTENSOR_PYTHON_LOCAL:-xtensor-python-0.24.1}" "${cmakeargs[@]}"
 
 }
 
-if [ $1 == "pybind11" ]; then
+if [ "$1" == "pybind11" ]; then
   pybind11
-elif [ $1 == "xtl" ]; then
+elif [ "$1" == "xtl" ]; then
   xtl
-elif [ $1 == "xsimd" ]; then
+elif [ "$1" == "xsimd" ]; then
   xsimd
-elif [ $1 == "xtensor" ]; then
+elif [ "$1" == "xtensor" ]; then
   xtensor
-elif [ $1 == "xtensor-blas" ]; then
+elif [ "$1" == "xtensor-blas" ]; then
   xtensor_blas
-elif [ $1 == "xtensor-python" ]; then
+elif [ "$1" == "xtensor-python" ]; then
   xtensor_python
-elif [ $1 == "everything" ]; then
+elif [ "$1" == "everything" ]; then
   pybind11
   xtl
   xsimd
