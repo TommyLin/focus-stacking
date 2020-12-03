1009b922f07fef51f76f4b8aa9cebd33d2bc938c
林宏文(Tommy Lin) <tommy.hw.lin@foxconn.com>
2020-12-03 16:13:49 +0800

    gnerator.py that generate two blurred images

---
+++ src/generator.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter
+
+filename = "../images/fly1.jpg"
+
+image0 = Image.open(filename)
+w, h = image0.size
+
+fig = plt.figure()
+ax = fig.add_subplot(3, 2, 1)
+ax.set_title("Original")
+plt.axis("off")
+plt.imshow(image0)
+
+
+image1 = image0.crop((0, 0, (w >> 1), h))
+print(image1.size)
+blurred1 = image1.filter(ImageFilter.GaussianBlur(radius=1))
+ax = fig.add_subplot(3, 2, 3)
+plt.imshow(blurred1)
+
+
+image2 = image0.crop(((w >> 1), 0, w, h))
+print(image2.size)
+blurred2 = image2.filter(ImageFilter.GaussianBlur(radius=1))
+ax = fig.add_subplot(3, 2, 4)
+plt.imshow(blurred2)
+
+
+image3 = Image.new("RGB", (w, h))
+image3.paste(blurred1, (0, 0, (w >> 1), h))
+image3.paste(image2, ((w >> 1), 0, w, h))
+
+ax = fig.add_subplot(3, 2, 5)
+plt.imshow(image3)
+
+
+image4 = Image.new("RGB", (w, h))
+image4.paste(image1, (0, 0, (w >> 1), h))
+image4.paste(blurred2, ((w >> 1), 0, w, h))
+
+ax = fig.add_subplot(3, 2, 6)
+plt.imshow(image4)
+
+plt.show()
