2a0739cbdd765c03c7df83a06d4398bdfd13e5ee
林宏文(Tommy Lin) <tommy.hw.lin@foxconn.com>
2020-12-03 17:42:59 +0800

    Add title to axes

--- src/generator.py
+++ src/generator.py
@@ -23,6 +23,8 @@ print(image1.size)
 blurred1 = image1.filter(ImageFilter.GaussianBlur(radius=1))
 if debug_show:
     ax = fig.add_subplot(rows, 2, 3)
+    ax.set_title("Left side")
+    plt.axis("off")
     plt.imshow(blurred1)
 
 
@@ -31,6 +33,8 @@ print(image2.size)
 blurred2 = image2.filter(ImageFilter.GaussianBlur(radius=1))
 if debug_show:
     ax = fig.add_subplot(rows, 2, 4)
+    ax.set_title("Right side")
+    plt.axis("off")
     plt.imshow(blurred2)
 
 
@@ -39,6 +43,7 @@ image3.paste(blurred1, (0, 0, (w >> 1), h))
 image3.paste(image2, ((w >> 1), 0, w, h))
 
 ax = fig.add_subplot(rows, 2, rows * 2 - 1)
+ax.set_title("Blurred left")
 plt.imshow(image3)
 
 
@@ -47,6 +52,7 @@ image4.paste(image1, (0, 0, (w >> 1), h))
 image4.paste(blurred2, ((w >> 1), 0, w, h))
 
 ax = fig.add_subplot(rows, 2, rows * 2)
+ax.set_title("Blurred right")
 plt.imshow(image4)
 
 plt.show()
