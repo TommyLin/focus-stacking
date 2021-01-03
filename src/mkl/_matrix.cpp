fcaa6ae51ffc1496e69180887423edb8a367d273
林宏文(Tommy Lin) <gnat_lin@hotmail.com>
2021-01-04 01:34:14 +0800

    Remove tile function

--- src/mkl/_matrix.cpp
+++ src/mkl/_matrix.cpp
@@ -37,7 +37,6 @@ public:
     ~Matrix() = default; // default destructor
 
     friend Matrix multiply_naive(const Matrix &mat1, const Matrix &mat2);
-    friend Matrix multiply_tile(const Matrix &mat1, const Matrix &mat2, const unsigned int tsize);
     friend Matrix multiply_mkl(const Matrix &mat1, const Matrix &mat2);
 
     size_t index(size_t row, size_t col) const { return row*m_ncol + col; }
@@ -81,56 +80,6 @@ Matrix multiply_naive(const Matrix &mat1, const Matrix &mat2)
     return ret;
 };
 
-Matrix multiply_tile(const Matrix &mat1, const Matrix &mat2, const unsigned int tsize)
-{
-    if (mat1.m_ncol != mat2.m_nrow)
-        throw std::invalid_argument("Input matrix rank didn't match");
-
-    if ((tsize == 0) || (tsize > mat1.m_nrow) || (mat1.m_nrow < 8))
-        return multiply_naive(mat1, mat2);
-
-    Matrix ret(mat1.m_nrow, mat2.m_ncol);
-    unsigned int remain = ret.m_nrow % 8;
-    size_t i, i_max = ret.m_nrow - remain;
-
-    for (i = 0; i < i_max; i += 8) {
-        for (size_t k = 0; k < ret.m_ncol; ++k) {
-            double v[8] = {0};
-            for (size_t j = 0; j < mat1.m_ncol; ++j) {
-                v[0] += mat1(i    , j) * mat2(j, k);
-                v[1] += mat1(i + 1, j) * mat2(j, k);
-                v[2] += mat1(i + 2, j) * mat2(j, k);
-                v[3] += mat1(i + 3, j) * mat2(j, k);
-                v[4] += mat1(i + 4, j) * mat2(j, k);
-                v[5] += mat1(i + 5, j) * mat2(j, k);
-                v[6] += mat1(i + 6, j) * mat2(j, k);
-                v[7] += mat1(i + 7, j) * mat2(j, k);
-            }
-            ret(i     , k) = v[0];
-            ret(i + 1 , k) = v[1];
-            ret(i + 2 , k) = v[2];
-            ret(i + 3 , k) = v[3];
-            ret(i + 4 , k) = v[4];
-            ret(i + 5 , k) = v[5];
-            ret(i + 6 , k) = v[6];
-            ret(i + 7 , k) = v[7];
-        }
-    }
-
-    if (remain) {
-        for (i = i_max; i < ret.m_nrow; ++i) {
-            for (size_t k = 0; k < ret.m_ncol; ++k) {
-                double v = 0;
-                for (size_t j = 0; j < mat1.m_ncol; ++j) {
-                    v += mat1(i, j) * mat2(j, k);
-                }
-                ret(i, k) = v;
-            }
-        }
-    }
-
-    return ret;
-};
 
 Matrix multiply_mkl(const Matrix &mat1, const Matrix &mat2)
 {
@@ -151,7 +100,6 @@ Matrix multiply_mkl(const Matrix &mat1, const Matrix &mat2)
 
 PYBIND11_MODULE(_matrix, m) {
     m.def("multiply_naive", &multiply_naive);
-    m.def("multiply_tile", &multiply_tile);
     m.def("multiply_mkl", &multiply_mkl);
     py::class_<Matrix>(m, "Matrix", py::buffer_protocol())
         .def(py::init<size_t, size_t>())
--- src/mkl/test_matrix.py
+++ src/mkl/test_matrix.py
@@ -3,7 +3,7 @@ import math
 import time
 import pytest
 
-from _matrix import Matrix, multiply_naive, multiply_tile, multiply_mkl
+from _matrix import Matrix, multiply_naive, multiply_mkl
 
 def execute(func, i, j, tsize):
     k = np.random.randint(100, 200)
@@ -12,10 +12,7 @@ def execute(func, i, j, tsize):
     mat1 = Matrix(np_mat1)
 
     mat2 = Matrix(np_mat2)
-    if func == multiply_tile:
-        result = func(mat1, mat2, tsize)
-    else:
-        result = func(mat1, mat2)
+    result = func(mat1, mat2)
 
     assert result.nrow == i
     assert result.ncol == j
@@ -23,26 +20,6 @@ def execute(func, i, j, tsize):
 
 def test_multiply_naive():
     execute(multiply_naive, 128, 128, 0)
-
-def test_multiply_tile1():
-    execute(multiply_tile, 128, 128, 0)
-    execute(multiply_tile, 128, 128, 15)
-    execute(multiply_tile, 128, 128, 16)
-    execute(multiply_tile, 128, 128, 17)
-    execute(multiply_tile, 128, 128, 256)
-
-def test_multiply_tile2():
-    execute(multiply_tile, 128, 127, 16)
-    execute(multiply_tile, 128, 128, 16)
-    execute(multiply_tile, 128, 129, 16)
-
-def test_multiply_tile3():
-    execute(multiply_tile, 127, 128, 16)
-    execute(multiply_tile, 128, 128, 16)
-    execute(multiply_tile, 129, 128, 16)
-
-def test_multiply_tile4():
-    execute(multiply_tile, 5, 5, 3)
 
 def test_multiply_mkl():
     execute(multiply_mkl, 128, 128, 0)
@@ -60,13 +37,6 @@ def test_performance():
         end = time.time()
         naive_timing.append(end - start)
 
-    tile_timing = []
-    for i in range(5):
-        start = time.time()
-        multiply_tile(mat1, mat2, 8)
-        end = time.time()
-        tile_timing.append(end - start)
-
     mkl_timing = []
     for i in range(5):
         start = time.time()
@@ -76,10 +46,7 @@ def test_performance():
 
     with open('performance.txt', 'w') as f:
         naivesec = np.min(naive_timing)
-        tilesec = np.min(tile_timing)
         mklsec = np.min(mkl_timing)
         print('multiply_naive runtime = {0:2.4f} seconds'.format(naivesec), file=f)
-        print('multiply_tile runtime = {0:2.4f} seconds'.format(tilesec), file=f)
         print('multiply_mkl runtime = {0:2.4f} seconds'.format(mklsec), file=f)
-        print('Tile speed-up over naive: {0:2.2f} x'.format(naivesec/tilesec), file=f)
         print('MKL speed-up over naive: {0:2.2f} x'.format(naivesec/mklsec), file=f)
